7f8acfe57f61402e4ba8223781b5e7861009f708
Sachin <007mishrasachinmishra@gmail.com>
2024-06-09 00:36:01 +0530

    Api setup and Redux functionality

---
+++ src/store/configureStore.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+
+const store = configureStore({
+    // reducer function - to generate functionality in parts 
+    reducer:{
+
+    }
+});
+
+export default store;
