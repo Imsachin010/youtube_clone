32c3461b9c09f9ad78391a11aa046d2f6dfb51e3
Sachin <007mishrasachinmishra@gmail.com>
2024-06-11 17:46:36 +0530

    working with redux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from "../components/Sidebar";
-
+import {useAppDispatch, useAppSelector} from "../hooks/useApp";
 export default function Home() {
+
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state)=> state.youtubeApp.videos);
   return (
     <div>
         <Navbar/>
--- src/store/reducers/getHomepageVideos.js
+++ src/store/reducers/getHomepageVideos.js
@@ -1,3 +1,18 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+export const getHomepageVideos = createAsyncThunk(
+    "youube/App/HomepageVideos",
+    async(isNext, {getState}) => {
+        const { 
+            youtubeApp: {nextPageToken : nextPagetokenFromState}, getHomepageVideos
+        } = getState();
+        const {
+            data = {items, nextPageToken},
+        } = await axios.get(`https://www.googleapis.com/youtube/v3/search?maxResults=20&q="drop x out"&key=${API_KEY}&part=snippet&type=video`);
+        
+        // const response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&key = ${API_KEY}${isNext ? `&pageToken=${nextPagetokenFromState}` : ""}`);
+    }
+)
