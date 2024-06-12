b2aeafd6338f27176ab742bed917a57a7f4a2dde
Sachin <007mishrasachinmishra@gmail.com>
2024-06-12 17:22:32 +0530

    createrd a loading spinner

---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div className="flex items-center w-full justify-center ">
+        <div className='h-10 w-10 borde-2 border-red-500 border-solid rounded-full animate-spin border-t-transparent'>
+        
+        </div>
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from "../components/Sidebar";
+import Spinner from '../components/Spinner'
 import { useAppDispatch, useAppSelector } from "../hooks/useApp";
 import { getHomepageVideos } from '../store/reducers/getHomepageVideos';
+import { useEffect } from 'react';
+
 
 export default function Home() {
 
@@ -11,12 +14,19 @@ export default function Home() {
 
   useEffect(() => {
     dispatch(getHomepageVideos(false));
+    console.log(videos);
   }, [dispatch]);
 
   return (
-    <div>
+    <div className='max-h-screen overflow-auto'>
+      <div style={{height:"7.5vh"}}>
       <Navbar/>
+      </div>
+      
+      <div className='flex' style={{height:"92.5vh"}}>
       <Sidebar/>
+      <Spinner/>
+      </div>
     </div>
   )
 };
