7189af17f91c413f9c82b3db5496be2db7762114
Sachin <007mishrasachinmishra@gmail.com>
2024-06-13 11:31:30 +0530

    designed cards of youtube

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,26 +2,29 @@ import React from 'react'
 
 export default function Card() {
   return (
-    <div>
-        <div>
-            <div>
-                <span>
-                    {data.videoDuration}
-                </span>
-                <img src={data.videoThumbnail} alt='thumbnail'/>
-            </div>
-            <div>
-                <div>
-                    <a href='#'> <img src={data.channelInfo.image} alt='channelimage'/>
+    <div className='w-64 h-60 gap-3 flex-col'>
+        <div className='relative '>
+            <span className='absolute bottom-3 right-3 text-sm bg-gray-900
+             py-0.5 px-2 z-10 '>
+                {data.videoDuration}
+            </span>
+            <img src={data.videoThumbnail} 
+            alt='thumbnail'
+            className="h-44 w-72 "/>
+            <div className='flex gap-2'>
+                <div className='min-w-fit'>
+                    <a href='#'> <img src={data.channelInfo.image} 
+                    alt='channelimage'
+                    className='h-9 w-9 rounded-full '/>
                     </a>
                 </div>
-                <div>
+                <div >
                     <h3>
-                        <a href='#' >{data.videoTitle}</a>
+                        <a href='#' className='line-clamp-2'>{data.videoTitle}</a>
                     </h3>
-                    <div>
+                    <div className='text-sm text-gray-500'>
                         <div>
-                            <a href='#'> {data.channelInfo.name}</a>
+                            <a href='#' className='hover:text-white'> {data.channelInfo.name}</a>
                         </div>
                         <div>
                             <span>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,7 +35,7 @@ export default function Home() {
           loader={<Spinner/>}
           height={650}
           >
-            <div>
+            <div className='grid gap-y-14 gap-x-8 grid-cols-4 p-8'>
               {videos.map((item) => {
                 return <Card data={item} key={item.videoId}/>
               })}
